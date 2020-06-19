e4988b62990c2f44c25381de6d04a124071c549e
Mariana Fantini <mariana.fantini@gmail.com>
2020-06-19 15:25:02 -0300

    Added branch input if user uses flag -j (jira)

--- commands/commit.js
+++ commands/commit.js
@@ -1,55 +1,67 @@
-const inquirer = require('inquirer'),
-    { exec } = require('child_process'),
-    authors = require('./authors');
+const inquirer = require("inquirer"),
+  { exec } = require("child_process"),
+  authors = require("./authors");
 
-module.exports = async function(opt) {
-    const authorList = await authors.get();
-    const prompts = [];
-    if (!opt.message) {
-        prompts.push({
-            type: 'input',
-            message: 'Commit message:',
-            name: 'commit',
-            validate: function(answer) {
-                return answer.trim() === ''? 'You should write a message for your commit' : true;
-            }
-        })
-    }
+module.exports = async function (opt) {
+  const authorList = await authors.get();
+  const prompts = [];
+  if (opt.j) {
     prompts.push({
-        type: 'checkbox',
-        message: 'Select authors:',
-        name: 'authors',
-        choices: authorList.map(author => {
-            return {
-                name: author.name,
-                value: author,
-                checked: author.default
-            };
-        }),
-        validate: function(answer) {
-            if (answer.length < 1) {
-                return 'You must choose at least one author.';
-            }
-            return true;
-        }
+      type: "input",
+      message: "Branch:",
+      name: "branch",
     });
-    const answers = await inquirer.prompt(prompts);
-    await authors.default(answers.authors);
-
-    let command = ['git commit'],
-        message = answers.commit || opt.message,
-        formattedAuthors = answers.authors.map(author => `Co-authored-by: ${author.name} <${author.email}>`).join('\\n');
-
-    if (Array.isArray(message)) {
-        message = message.map(m => `${m}`).join('\\n');
-    }
-
-    if (opt.all) {
-        command.push('-a');
-    }
-    command.push(`-m$'${message}\\n\\n${formattedAuthors}'`);
-
-    exec(command.join(' '), (err, stdout, stderr) => {
-        console.log(`${stdout}`);
+  }
+  if (!opt.message) {
+    prompts.push({
+      type: "input",
+      message: "Commit message:",
+      name: "commit",
+      validate: function (answer) {
+        return answer.trim() === ""
+          ? "You should write a message for your commit"
+          : true;
+      },
     });
+  }
+  prompts.push({
+    type: "checkbox",
+    message: "Select authors:",
+    name: "authors",
+    choices: authorList.map((author) => {
+      return {
+        name: author.name,
+        value: author,
+        checked: author.default,
+      };
+    }),
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return "You must choose at least one author.";
+      }
+      return true;
+    },
+  });
+  const answers = await inquirer.prompt(prompts);
+  await authors.default(answers.authors);
+
+  let command = ["git commit"],
+    message = answers.commit || opt.message,
+    branch = answers.branch ? `[${answers.branch}] ` : ``,
+    formattedAuthors = answers.authors
+      .map((author) => `Co-authored-by: ${author.name} <${author.email}>`)
+      .join("\\n");
+
+  if (Array.isArray(message)) {
+    message = message.map((m) => `${m}`).join("\\n");
+  }
+
+  if (opt.all) {
+    command.push("-a");
+  }
+  command.push(`-m$'${branch}${message}\\n\\n${formattedAuthors}'`);
+
+  exec(command.join(" "), (err, stdout, stderr) => {
+    console.log(`${stdout}`);
+  });
 };
