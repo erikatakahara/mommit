4b1e94b58b38e0915855578f85c331ca9ec6c925
Erika Takahara <erika.takahara@gmail.com>
2020-09-14 01:38:24 -0300

    feat(*): add linter

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 12
+	},
+    "globals": {
+        "require": "readonly",
+        "module": "writable"
+    },
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
